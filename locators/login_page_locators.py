c14833954c7cc8252d8660c8e9011baa4bc911e9
Olga Dmitrieva <olgadmitrrr@yandex.ru>
2026-05-03 19:35:40 +0300

    Добавила allure step, проверку имени пользователя, перенесла локаторы и методы в корретные страницы. Создала Iogin_page_locators.

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     """Локаторы страницы ввода логина и пароля ArtyShop"""
 
- # Кнопка "Войти"
+  # Кнопка "Войти"
     login_button = (By.XPATH, f"//button[normalize-space()='Войти']")
 
- # Поле ввода E-Mail на странице авторизации
+  # Поле ввода E-Mail на странице авторизации
     login_field = (By.XPATH, f"//input[@id='input-email']")
 
- # Поле ввода пароля на странице авторизации
+  # Поле ввода пароля на странице авторизации
     password_field = (By.XPATH, f"//input[@id='input-password']")
