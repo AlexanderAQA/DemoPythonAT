4e6927d5c7201a5e3c1a69eb2cfe1e19c0c910c7
Olga Dmitrieva <olgadmitrrr@yandex.ru>
2026-06-01 14:27:02 +0300

    добавила тест проверки инфо пользователя, локаторы, проверки

--- locators/account_page_locators.py
+++ locators/account_page_locators.py
@@ -15,3 +15,61 @@ class AccountPageLocators:
         locator = (By.XPATH, f"//span[normalize-space()='{text}']")
 
         return locator
+
+    # Локатор кнопки "Изменить информацию"
+    CHANGE_INFORMATION_BUTTON =  (By.XPATH, "//a[normalize-space()='Изменить информацию']")
+
+    # Локатор заголовка Ваша учетная запись
+    YOUR_ACCOUNT =  (By.XPATH, "//legend[normalize-space()='Ваша учетная запись']")
+
+    # Локатор названия поля "Имя, Отчество"
+    NAME_PATRONYMIC =  (By.XPATH, "//label[@for='input-firstname']")
+
+    # Локатор названия поля "Фамилия"
+    SURNAME =  (By.XPATH, "//label[@for='input-lastname']")
+
+    # Локатор названия поля "E-Mail"
+    EMAIL =  (By.XPATH, "//label[@for='input-email']")
+
+    # Локатор названия поля "Телефон"
+    TELEPHONE =  (By.XPATH, "//label[@for='input-telephone']")
+
+    # Локатор текста в поле "Имя"
+    @staticmethod
+    def get_text_in_name_field(name: str):
+            locator = (By.XPATH, f"//input[@id='input-firstname' and @value='{name}']")
+
+            return locator
+
+    # Локатор текста в поле "Фамилия"
+    @staticmethod
+    def get_text_in_surname_field(surname: str):
+            locator = (By.XPATH, f"//input[@id='input-lastname' and contains(@value, '{surname}')]")
+
+            return locator
+
+    # Локатор текста в поле "E-mail"
+    @staticmethod
+    def get_text_in_email_field(email: str):
+            locator = (By.XPATH, f"//input[@id='input-email' and @value='{email}']")
+
+            return locator
+
+    # Локатор текста в поле "Телефон"
+    @staticmethod
+    def get_text_in_telephone_field(tel: str):
+            locator = (By.XPATH, f"//input[@id='input-telephone' and @value='{tel}']")
+
+            return locator
+
+    # Локатор кнопки "Назад"
+    BACK_BUTTON =  (By.XPATH, "//a[normalize-space()='Назад']")
+
+    # Локатор кнопки "Продолжить"
+    CONTINUE_BUTTON =  (By.XPATH, "//button[normalize-space()='Продолжить']")
+
+    # Локатор кнопки "Личный кабинет"
+    PERS_ACCOUNT =  (By.XPATH, "//ul[@class='breadcrumb']//a[normalize-space()='Личный Кабинет']")
+
+    # Локатор кнопки "Домой"
+    HOME =  (By.XPATH, "//li[@class='breadcrumb-item']/a[i[@class='fas fa-home']]")
--- pages/account_page.py
+++ pages/account_page.py
@@ -27,3 +27,42 @@ class AccountPage(BasePage):
             self.asserts.assert_is_equal(user.name, actual_name)
 
             return self
+
+    def click_change_information(self):
+        with allure.step("Клик по кнопке 'Изменить информацию'"):
+            self.click(AccountPageLocators.CHANGE_INFORMATION_BUTTON)
+
+        return self
+
+    def assert_field_headers(self):
+        with allure.step("Проверка наличия всех основных заголовков полей на странице"):
+            self.wait_for_element(AccountPageLocators.YOUR_ACCOUNT)
+            self.wait_for_element(AccountPageLocators.NAME_PATRONYMIC)
+            self.wait_for_element(AccountPageLocators.SURNAME)
+            self.wait_for_element(AccountPageLocators.EMAIL)
+            self.wait_for_element(AccountPageLocators.TELEPHONE)
+
+        return self
+
+    def assert_profile_field_values(self):
+        with allure.step("Проверка значений во всех полях профиля"):
+            self.wait_for_element(AccountPageLocators.get_text_in_name_field("Ольга"))
+            self.wait_for_element(AccountPageLocators.get_text_in_surname_field("Тверская"))
+            self.wait_for_element(AccountPageLocators.get_text_in_email_field("helgaautotests@gmail.com"))
+            self.wait_for_element(AccountPageLocators.get_text_in_telephone_field("+7 (900) 000-00-00"))
+
+        return self
+
+    def assert_back_continue_buttons(self):
+        with allure.step("Проверка наличия кнопок 'Назад' и 'Продолжить'"):
+            self.wait_for_element(AccountPageLocators.BACK_BUTTON)
+            self.wait_for_element(AccountPageLocators.CONTINUE_BUTTON)
+
+        return self
+
+    def assert_pers_account_and_home_buttons(self):
+        with allure.step("Проверка наличия кнопок `Домой` и `Личный кабинет`"):
+            self.wait_for_element(AccountPageLocators.HOME)
+            self.wait_for_element(AccountPageLocators.PERS_ACCOUNT)
+
+        return self
