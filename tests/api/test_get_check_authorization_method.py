7869b62068fabe6cef1c52c94a09b77967a8915e
Olga Dmitrieva <olgadmitrrr@yandex.ru>
2026-06-22 22:57:51 +0300

    Поправила проверки (заменила на те что есть в assertions)

--- tests/api/test_get_check_authorization_method.py
+++ tests/api/test_get_check_authorization_method.py
@@ -2,38 +2,42 @@ import pytest
 import allure
 from src.utils.test_data import USER_OLGA
 
+class TestAuthMethod:
 
-@pytest.mark.positive
-@pytest.mark.api
-@allure.title("Авторизация пользователя по customer_token")
-@allure.link("https://testit.example.com/tc-auth-token")
-def test_get_check_authorization_method(api_client):
-    """Проверка авторизации через GET запрос с customer_token"""
+    @pytest.mark.positive
+    @pytest.mark.api
+    @allure.title("Авторизация пользователя по customer_token")
+    def test_get_check_authorization_method(self, api_client):
+        """Проверка авторизации через GET запрос с customer_token"""
 
-    user = USER_OLGA
+        user = USER_OLGA
 
-    # логин (вход)
-    customer_token, login_status = api_client.login(
-        email=user.login,
-        password=user.password
-    )
+        # логин (вход)
+        customer_token, login_status = api_client.login(
+            email=user.login,
+            password=user.password
+        )
 
-    # проверка успешного логина
-    assert login_status == 200, f"Login failed with status {login_status}"
-    assert customer_token, "Customer token not received after login"
+        # Проверка, что есть токен
+        assert customer_token, "Customer token not received after login"
 
-    # проверка авторизации в личном кабинете
-    expected_username = user.name
-    expected_page_title = "Личный Кабинет"
-    expected_token_param = f"customer_token={customer_token}"
-    expected_logout_link = "route=account/logout"
+        # проверка успешного логина
+        (api_client.assertions
+         .assert_is_equal(200, login_status))
 
-    response_html, status_code = api_client.get_account_page(customer_token=customer_token)
+        # Данные для проверки
+        expected_username = user.name
+        expected_page_title = "Личный Кабинет"
+        expected_token_param = f"customer_token={customer_token}"
+        expected_logout_link = "route=account/logout"
 
-    # проверки ответа и ожидаемых параметров
-    assert status_code == 200
-    assert expected_page_title in response_html
-    assert expected_username in response_html
-    assert expected_token_param in response_html
-    assert expected_logout_link in response_html
+        # запрос страницы ЛК клиента
+        response_html, status_code = api_client.get_account_page(customer_token=customer_token)
 
+        # проверки ответа и ожидаемых параметров - код 200, заголовок, имя пользователя, токен в ссылке, ссылка выхода
+        (api_client.assertions
+         .assert_is_equal(200, status_code)
+         .assert_text_match(expected_page_title, response_html)
+         .assert_text_match(expected_username, response_html)
+         .assert_text_match(expected_token_param, response_html)
+         .assert_text_match(expected_logout_link, response_html))
